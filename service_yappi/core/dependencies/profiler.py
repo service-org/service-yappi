1e8072986e9809bd8a701e80c23563c7f10acbab
forcemain <forcemain@163.com>
2021-09-28 16:41:27 +0800

    :underage:

--- service_yappi/core/dependencies/profiler.py
+++ service_yappi/core/dependencies/profiler.py
@@ -29,7 +29,7 @@ class Profiler(Dependency):
     def __init__(
             self,
             alias: t.Text,
-            profile_options: t.Optional[t.Dict[t.Text, t.Any]] = None,
+            initial_options: t.Optional[t.Dict[t.Text, t.Any]] = None,
             getfunc_options: t.Optional[t.Dict[t.Text, t.Any]] = None,
             **kwargs: t.Any
     ) -> None:
@@ -37,11 +37,11 @@ class Profiler(Dependency):
 
         @param alias: 配置别名
         @param getfunc_options: 过滤配置
-        @param profile_options: 采集配置
+        @param initial_options: 采集配置
         @param kwargs: 其它配置
         """
         self.alias = alias
-        self.profile_options = profile_options or {}
+        self.initial_options = initial_options or {}
         self.getfunc_options = getfunc_options or {}
         super(Profiler, self).__init__(**kwargs)
 
@@ -50,9 +50,9 @@ class Profiler(Dependency):
 
         @return: None
         """
-        profile_options = self.container.config.get(f'{YAPPI_CONFIG_KEY}.{self.alias}.profile_options', default={})
+        initial_options = self.container.config.get(f'{YAPPI_CONFIG_KEY}.{self.alias}.initial_options', default={})
         # 防止YAML中声明值为None
-        self.profile_options = (profile_options or {}) | self.profile_options
+        self.initial_options = (initial_options or {}) | self.initial_options
         getfunc_options = self.container.config.get(f'{YAPPI_CONFIG_KEY}.{self.alias}.getfunc_options', default={})
         # 防止YAML中声明值为None
         self.getfunc_options = (getfunc_options or {}) | self.getfunc_options
@@ -67,7 +67,7 @@ class Profiler(Dependency):
         @param context: 上下文对象
         @return: None
         """
-        yappi.start(**self.profile_options)
+        yappi.start(**self.initial_options)
 
     def worker_finish(self, context: WorkerContext) -> None:
         """ 工作协程 - 完毕回调
@@ -79,7 +79,7 @@ class Profiler(Dependency):
         ystats = yappi.get_func_stats(**self.getfunc_options)
         pstats = yappi.convert2pstats(ystats)
         base_name = f'{int(time.time() * 1000000)}.prof'
-        file_path = os.path.join(tempfile.gettempdir(), 'profile')
+        file_path = os.path.join(tempfile.gettempdir(), 'profiler')
         os.path.exists(file_path) or os.makedirs(file_path, exist_ok=True)
         file_name = os.path.join(file_path, base_name)
         pstats.dump_stats(file_name)
